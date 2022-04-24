81d4b7ad493a38b8b33aa8f83cefca79a6162767
Vermouth <qhddusl2004@naver.com>
2022-04-25 01:06:50 +0900

    테스트 케이스를 만족하는지 확인한다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -14,6 +15,7 @@ class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
+    @DisplayName("전진, 정지")
     void 전진_정지() {
         assertRandomNumberInRangeTest(
             () -> {
@@ -25,6 +27,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    @DisplayName("이름에 대한 예외 처리")
     void 이름에_대한_예외_처리() {
         assertSimpleTest(
             () -> {
